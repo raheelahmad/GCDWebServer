2b7929ff293a853a34aecc84d4a9dcda29e66a83
Raheel Ahmad <ahmadraheel@gmail.com>
2023-04-25 17:26:06 -0700

    Add GCDWebUploader

--- Package.swift
+++ Package.swift
@@ -5,11 +5,20 @@ let package = Package(
     name: "GCDWebServer",
     products: [
         .library(name: "GCDWebServer", targets: ["GCDWebServer"]),
+        .library(name: "GCDWebUploader", targets: ["GCDWebUploader"]),
     ],
     targets: [
         .target(
             name: "GCDWebServer",
             path: "GCDWebServer",
+            cSettings: [
+                .define("SWIFT_PACKAGE")
+            ]
+        ),
+        .target(
+            name: "GCDWebUploader",
+            dependencies: ["GCDWebServer"],
+            path: "GCDWebUploader",
             cSettings: [
                 .define("SWIFT_PACKAGE")
             ]
